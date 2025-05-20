d8ad67a28d47554d946ec12edb334325e3c6bb4e
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-20 02:25:41 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int n, a = 0, b = 1, suivant;
-    scanf("%d", &n); // Saisie de l'utilisateur (ex: 20)
+    int n, a = 0, b = 1;
+    scanf("%d", &n); // L'utilisateur saisit le nombre de termes (ex: 20)
 
-    // Affiche les deux premiers termes (0 et 1)
-    printf("%d %d ", a, b);
-
-    // Génère les (n - 2) termes suivants
-    for (int i = 2; i < n; i++) {
-        suivant = a + b;
-        printf("%d ", suivant);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        int temp = a;
         a = b;
-        b = suivant;
+        b = temp + b;
     }
 
     return 0;
