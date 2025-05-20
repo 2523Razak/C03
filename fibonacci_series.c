30babe0530de8c9d5f1cc3a62aee85feaaff865b
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-20 02:35:18 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int limite;
-    scanf("%d", &limite);  // L'utilisateur entre la limite (ex: 20)
+    int max;
+    scanf("%d", &max);
     
-    int a = 0, b = 1;      // On initialise les 2 premiers termes
+    if (max >= 0) printf("0");
+    if (max >= 1) printf("1");
     
-    // On affiche toujours le premier 0
-    printf("%d ", a);
-    
-    // On affiche les termes suivants tant qu'ils sont <= limite
-    while (b <= limite) {
-        printf("%d ", b);
-        int prochain = a + b;
-        a = b;
-        b = prochain;
+    int prev = 0, current = 1;
+    while (1) {
+        int next = prev + current;
+        if (next > max) break;
+        printf("%d ", next);
+        prev = current;
+        current = next;
     }
     
     return 0;
