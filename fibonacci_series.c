0bf49942eddc334d0ea423daadc2eb7b719e4442
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-25 01:55:02 +0100

    Update fibonacci_series.c

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
+// Ce script permet d'afficher la série de Fibonacci jusqu'à une valeur limite donnée
+
 int main() {
-    int n;
-    scanf("%d", &n);  // Lecture de la limite
-    
-    int a = 0, b = 1; // Initialisation des termes
-    printf("%d", a);   // Affiche toujours U0=0
-    
-    // Affiche les termes <= n avec espace avant (sauf premier)
-    while (b <= n) {
-        printf(" %d", b);
-        int suivant = a + b;
+    // Initialisation des variables
+    int limite;
+    int a = 0, b = 1, suivant;
+
+    // Récupération de la valeur entrée par l'utilisateur
+    scanf("%d", &limite);
+
+    // Affichage de la première valeur
+    printf("Fibonacci : %d ", a);
+
+    // Boucle et conditions pour vérifier la logique de Fibonacci et afficher les valeurs
+    while (b <= limite) {
+        printf("%d ", b);
+        suivant = a + b;
         a = b;
         b = suivant;
     }
-    
+    printf("\n");
+
     return 0;
 }
